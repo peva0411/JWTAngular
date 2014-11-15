4a18fbdb33daf99b66bbedc58aef1529fcdf117d
Phil <peva0411@gmail.com>
2014-11-15 10:28:23 -0500

    init commit

---
+++ PsJwt.Web/Angular/app.config.js
@@ -0,0 +1,14 @@
+﻿angular.module('psJwtApp').config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('main', {
+            url: '/',
+            templateUrl: '/Angular/views/main.html'
+        })
+        .state('register', {
+        url: '/register',
+        templateUrl: '/Angular/views/register.html'
+    });
+})
---
+++ PsJwt.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PsJwt.Web.Startup))]
+namespace PsJwt.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
